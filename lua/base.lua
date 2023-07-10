db1ee433987ab1d09028ad9b9e3087d8c4ecba4f
DNIIBOY <daniel.nettelfield@gmail.com>
2023-07-10 16:51:01 +0200

    tab to 4 spaces

--- lua/base.lua
+++ lua/base.lua
@@ -13,9 +13,9 @@ opt.sidescrolloff = 4
 opt.hlsearch = true
 opt.incsearch = true
 
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 
 opt.ignorecase = true
