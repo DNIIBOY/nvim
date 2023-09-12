9748b66f9a3ccc264db8e1add788ea691de0869d
DNIIBOY <daniel.nettelfield@gmail.com>
2023-09-12 08:41:27 +0200

    new telescope binds

--- lua/p-telescope.lua
+++ lua/p-telescope.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fr', builtin.resume, {})
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
 
 
 local actions = require('telescope.actions')
