7fe651b9274ebe64d0e3bcffa8b85c4c833d0c24
DNIIBOY <daniel.nettelfield@gmail.com>
2024-01-25 15:12:47 +0100

    set tab to autocomplete

--- lua/p-lsp.lua
+++ lua/p-lsp.lua
@@ -5,10 +5,9 @@ lsp.preset('recommended')
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<Return>'] = cmp.mapping.confirm({ select = true }),
 })
 
 cmp.setup({
